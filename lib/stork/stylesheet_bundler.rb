db672b9fc431a65e9392d060f614705379cb4aad
Avand Amiri <avand@avandamiri.com>
2011-02-02 15:48:25 -0600

    Stork::StylesheetBundler now tested and #call simplified

--- lib/stork/stylesheet_bundler.rb
+++ lib/stork/stylesheet_bundler.rb
@@ -2,14 +2,17 @@ require 'sass'
 
 module Stork
   class StylesheetBundler
-    class << self
-      def call(env)
-        [200, {}, [bundle(env.params)]]
-      end
+    def call(env)
+      status, headers, body = 200, {}, bundle(env.params)
+    rescue
+    ensure
+      [status, headers, body]
+    end
 
-      def bundle(params)
-        Sass::Engine.new(File.open("#{Stork::Config.css_dir}/#{params[:package]}.scss", 'r').read, Stork::Config.sass_options).render
-      end
+  private
+
+    def bundle(params)
+      Sass::Engine.new(File.open("#{Stork::Config.css_dir}/#{params['package']}.scss", 'r').read, Stork::Config.sass_options).render
     end
   end
 end
--- test/stylesheet_bundler_test.rb
+++ test/stylesheet_bundler_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'rack'
 
 class StylesheetBundlerTest < Test::Unit::TestCase
   def setup
@@ -6,8 +7,17 @@ class StylesheetBundlerTest < Test::Unit::TestCase
     Stork::Config.css_dir = @fixtures_path
   end
 
+  def test_call_returns_successful_response
+    r = Rack::Request.new Rack::MockRequest.env_for "/?package=stylesheet"
+    response = Stork::StylesheetBundler.new.call r
+
+    assert_equal 200, response[0]
+    assert_equal({}, response[1])
+    assert response[2].is_a?(String)
+  end
+
   def test_bundle_returns_compiled_scss_with_default_options
     compiled_sass = compile_scss File.join(@fixtures_path, 'stylesheet.scss'), Stork::Config.sass_options
-    assert_equal compiled_sass, Stork::StylesheetBundler.bundle(:package => 'stylesheet')
+    assert_equal compiled_sass, Stork::StylesheetBundler.new.send(:bundle, 'package' => 'stylesheet')
   end
 end
